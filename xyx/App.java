bfc18f458cddf854fc16c627aebf48267ed17156
rootzxc <145563378+rootzxc@users.noreply.github.com>
2023-09-20 20:04:44 +0800

    Add files via upload

---
+++ xyx/App.java
@@ -0,0 +1,13 @@
+import com.itheima.ui.GameJFrame;
+import com.itheima.ui.LoginJFrame;
+import com.itheima.ui.RegisterJframe;
+
+public class App {
+    public static void main(String[] args) {
+        //表示程序的启动入口
+
+        
+
+        new GameJFrame();
+    }
+}
